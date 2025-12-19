666f05b48c1976f817438c27348f2e4bf4cefa73
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-12-19 15:23:08 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,27 @@
 // vue.config.js
 const path = require('path')
+
 module.exports = {
   outputDir: 'dist',
-  publicPath: './', // КРИТИЧНО для Cloudflare Pages!
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src')
-      }
-    }
-  }
+  publicPath: './',
+  
+  chainWebpack: config => {
+    config.resolve.alias.set('@', path.resolve(__dirname, 'src'))
+    
+    // Правило для .mjs файлов
+    config.module
+      .rule('mjs')
+      .test(/\.mjs$/)
+      .include
+        .add(/node_modules/)
+        .end()
+      .type('javascript/auto')
+  },
+  
+  // Транспиляция всех проблемных пакетов
+  transpileDependencies: [
+    /node_modules[\\/]iceberg-js/,
+    /node_modules[\\/]@supabase[\\/]storage-js/,
+    /node_modules[\\/]@supabase[\\/]supabase-js/
+  ]
 }
