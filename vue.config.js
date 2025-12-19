dfb4f221f1ba0683411ec4d26dfdbb040a5a4c7e
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-12-19 16:21:14 +0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -4,27 +4,31 @@ const path = require('path')
 module.exports = {
   outputDir: 'dist',
   publicPath: './',
-  configureWebpack: {
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, 'src')
-      }
-    },
-    // КРИТИЧНО: Правило для файлов .mjs из node_modules
-    module: {
-      rules: [
-        {
-          test: /\.mjs$/,
-          include: /node_modules/,
-          type: 'javascript/auto'
-        }
-      ]
-    }
-  },
-  // КРИТИЧНО: Транспиляция конкретных проблемных пакетов
-  transpileDependencies: [
-    'iceberg-js',
-    /@supabase\/storage-js/,
-    /@supabase\/supabase-js/
-  ]
+
+  // Ключевое решение: прямое правило для chainWebpack
+  chainWebpack: (config) => {
+    // Стандартный алиас
+    config.resolve.alias.set('@', path.resolve(__dirname, 'src'))
+
+    // 1. ОБЯЗАТЕЛЬНОЕ правило для файлов .mjs из node_modules
+    config.module
+      .rule('mjs')
+        .test(/\.mjs$/)
+        .include
+          .add(/node_modules/)
+          .end()
+        .type('javascript/auto');
+
+    // 2. ЯВНОЕ правило для транспиляции iceberk-js и supabase через babel-loader
+    // Это переопределяет стандартные настройки и гарантирует обработку.
+    config.module
+      .rule('js')
+        .test(/\.js$/)
+        .include
+          .add(/node_modules[\\/](iceberg-js|@supabase[\\/](storage-js|supabase-js))/)
+          .end()
+        .use('babel-loader')
+          .loader('babel-loader')
+          .end();
+  }
 }
