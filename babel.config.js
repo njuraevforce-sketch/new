c3aab77f47a0ae225804d94e37fcd8e8fe135677
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-12-19 19:23:48 +0500

    Delete babel.config.js

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-// babel.config.js
-module.exports = {
-  presets: [
-    '@vue/cli-plugin-babel/preset'
-  ],
-  plugins: [
-    '@babel/plugin-proposal-optional-chaining' // Этот плагин обрабатывает оператор `?.`
-  ]
-}
