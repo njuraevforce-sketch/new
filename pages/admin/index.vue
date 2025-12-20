cdcc1a0c169867ae25e28e3053c6d9704eee1b1e
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-12-20 18:25:27 +0500

    Create index.vue

---
+++ pages/admin/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <view class="admin-page">
+    <view class="admin-header">
+      <h1>Admin Panel</h1>
+    </view>
+    <view class="admin-content">
+      <!-- Add admin content here -->
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'AdminPage'
+}
+</script>
