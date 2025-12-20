70de5f7a4f00f994de1f5f5d9f6b9c9cc42f324d
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-12-20 23:56:25 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,54 +1,30 @@
 import { defineConfig } from 'vite'
-import uni from '@dcloudio/vite-plugin-uni'
-import { resolve } from 'path'
+import vue from '@vitejs/plugin-vue'
+import uni from '@uni-helper/vite-plugin-uni'
+import Components from 'unplugin-vue-components/vite'
 
 export default defineConfig({
-  plugins: [uni()],
-  base: './',
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './'),
-      '@components': resolve(__dirname, './components'),
-      '@pages': resolve(__dirname, './pages'),
-      '@utils': resolve(__dirname, './utils'),
-      '@stores': resolve(__dirname, './stores'),
-      '@static': resolve(__dirname, './static')
-    }
-  },
+  plugins: [
+    vue(),
+    uni(),
+    Components({
+      dts: true,
+    })
+  ],
   build: {
     outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    minify: 'terser',
-    target: 'es2015',
+    assetsDir: 'static',
     rollupOptions: {
       output: {
         manualChunks: {
-          'vendor': ['vue', 'vue-router', 'pinia'],
-          'supabase': ['@supabase/supabase-js']
-        },
-        chunkFileNames: 'assets/js/[name]-[hash].js',
-        entryFileNames: 'assets/js/[name]-[hash].js',
-        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]'
-      }
-    },
-    terserOptions: {
-      compress: {
-        drop_console: true,
-        drop_debugger: true
+          vendor: ['vue', 'pinia', 'vue-router'],
+          supabase: ['@supabase/supabase-js']
+        }
       }
     }
   },
   server: {
     port: 3000,
-    host: true,
-    open: true
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "@/uni.scss";`
-      }
-    }
+    host: true
   }
 })
