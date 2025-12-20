af58b53e00bb4436d65621a9a232184d06477ec9
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-12-20 18:26:10 +0500

    Create index.vue

---
+++ pages/backend/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="backend-login">
+    <view class="login-form">
+      <h1>Admin Login</h1>
+      <!-- Add admin login form here -->
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'BackendPage'
+}
+</script>
