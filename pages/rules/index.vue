791b9a91bc53b7bf89d04f5736730561d6db15b7
njuraevforce-sketch <n.juraevforce@gmail.com>
2025-12-20 18:25:48 +0500

    Create index.vue

---
+++ pages/rules/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <view class="rules-page">
+    <view class="rules-content">
+      <h1>Platform Rules</h1>
+      <!-- Add rules content here -->
+    </view>
+  </view>
+</template>
+
+<script>
+export default {
+  name: 'RulesPage'
+}
+</script>
